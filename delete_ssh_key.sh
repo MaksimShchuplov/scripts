a8eb7af49bc15b6f6e331cec151c644fe7726999
Maksim Shchuplov <shchuplov@gmail.com>
2018-08-13 19:27:52 +0300

    Update delete_ssh_key.sh

--- delete_ssh_key.sh
+++ delete_ssh_key.sh
@@ -4,7 +4,11 @@ key_dir="group"
 key_rep="group/keys"
 key=$1
 
-test -z "$key" && exit 1
+if echo "$key_id" |awk '/^ssh/{e=1};length($0)<30{e=1};/[[:space:]]+/{e=1};/\@/{e=1}END{if(e)exit(0);else exit(1);}'; then
+        echo "ERROR: wrong key"
+        exit 1
+fi
+
 
 grep -l "$key" $key_dir/* | while read file; do
         echo -ne "delete key $key from file $file\n"
